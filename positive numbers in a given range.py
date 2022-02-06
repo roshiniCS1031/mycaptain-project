f40bfaccd2835ae15b3ae3f39c0cf6c788aeaa51
roshiniCS1031 <82582365+roshiniCS1031@users.noreply.github.com>
2022-02-06 20:52:30 +0530

    positive numbers in a given range

---
+++ positive numbers in a given range.py
@@ -0,0 +1,8 @@
+list1 = [12, -7, 5, 64, -14]
+print("Original numbers in the list: ",list1)
+new_list1 = list(filter(lambda x: x >0, list1))
+print("Positive numbers in list1: ",new_list1)
+list2 = [12, 14, -95, 3] 
+print("Original numbers in the list: ",list2)
+new_list2 = list(filter(lambda x: x >0, list2))
+print("Positive numbers in  list2: ",new_list2)
