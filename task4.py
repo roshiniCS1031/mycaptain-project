f53dae14b403d6aaa87b48d0180a578fd1b189f7
roshiniCS1031 <82582365+roshiniCS1031@users.noreply.github.com>
2022-02-10 15:38:43 +0530

    task 4

---
+++ task4.py
@@ -0,0 +1,9 @@
+# Program to perform different set operations like in mathematics
+
+# define three sets
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+print("Union of E and N is",E | N)
+print("Intersection of E and N is",E & N)
+print("Difference of E and N is",E - N)
+print("Symmetric difference of E and N is",E ^ N)
